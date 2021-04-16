3c8ed2d474df532c2d40bb7a97c8d03386b150c0
261705 <72786686+261705@users.noreply.github.com>
2021-04-16 08:45:17 +0530

    Update header.h

--- 3_Implementation/inc/header.h
+++ 3_Implementation/inc/header.h
@@ -1,7 +1,7 @@
 /**
  * @file header.h
  * @author Navaneeth Reddy (navaneethgopireddy@gmail.com)
- * @brief 
+ * @brief A program to calculate income tax of an employee.
  * @version 0.1
  * @date 2021-04-10
  * 
@@ -10,12 +10,15 @@
  */
 /**
  * @brief 
- * 
+ * enum structure to store frequently used constants
  */
 typedef enum error
 {
     ZERO_TAX=0,ZERO_DEDUCTIONS=0
 }error;
+/**
+* @breif structure details to store the entire tax details
+*/
 typedef struct details
 {
 char name[30];
@@ -49,55 +52,72 @@ double taxable_income;
 double income_tax;
 } details;
 /**
- * @brief 
- * 
+ * @brief it takes user input data from main function and invokes to structure members.
+ * @param[in] details structure
+ * @param[in] n variable to invoke into structure name variable.
+ * @param[in] f variable to be invoked into structure financial_year variable.
+ * @param[in] P variable to be invoked into structure PAN variable.
+ * @param[in] a variable to be invoked into structure age variable.
+ * @param[in] ifg variable to be invoked into structure i_f_salary variable.
+ * @param[in] ifp variable to be invoked into structure i_f_property variable.
+ * @param[in] ifb variable to be invoked into structure i_f_business variable.
+ * @param[in] ifg variable to be invoked into structure i_f_gains variable.
+ * @param[in] ifo variable to be invoked into structure i_f_othersources variable.
+ * @param[in] ins variable to be invoked into structure life_insurance variable.
+ * @param[in] pa variable to be invoked into structure pension_allowance variable.
+ * @param[in] iobs variable to be invoked into structure i_o_savings_bank_acc variable.
+ * @param[in] ioe variable to be invoked into structure i_o_eduloan variable.
+ * @param[in] ioh variable to be invoked into structure i_o_homeloan variable.
+ * @param[in] mi variable to be invoked into structure medical_insurance variable.
+ * @param[in] me variable to be invoked into structure medical_expenditure variable.
+ * @param[in] ctp variable to be invoked into structure contribution_to_political_party variable.
  * @return details 
  */
 details input_details(details,char[],char[],char[],double,double,double,double,double,double,double,double,double,double,double,double,double,double);
 /**
- * @brief 
+ * @brief function that calculates gross income of an employee
  * 
  * @return details 
  */
 details gross_income(details);
 /**
- * @brief 
+ * @brief function that calculates deductions of an employee
  * 
  * @return details 
  */
 details deductions(details);
 /**
- * @brief 
+ * @brief function that calculates tax of an employee.
  * 
  * @return details 
  */
 details tax(details);
 /**
- * @brief 
+ * @brief function that generates report of tax details.
  * 
  * @return details 
  */
 details report_generator(details);
 /**
- * @brief 
+ * @brief function that used to test gross_income function.
  * 
  * @return details 
  */
 void test_gross_income(void);
 /**
- * @brief 
+ * @brief function used to test deductions function.
  * 
  * @return details 
  */
 void test_deductions(void);
 /**
- * @brief 
+ * @brief function used to test tax function.
  * 
  * @return details 
  */
 void test_tax(void);
 /**
- * @brief 
+ * @brief fumction used to test input_details function.
  * 
  * @return details 
  */
